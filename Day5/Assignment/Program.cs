55eb1d5254d19d135c8dd0d12ae055887dd18a74
Wei Yue <genius0610@gmail.com>
2018-11-09 11:12:12 +0800

    Add file via upload (Day5.part2)

---
+++ Day5/Assignment/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //求5 - 100所有质数的和，应该是 1055.
+            int sum = 0;
+            for (int i = 5; i < 101; i++)
+            {
+                bool prime = true;
+                for (int j = 2; j < i / j + 1; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime == true)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine("5 ~ 100 以内的质数和为 {0}.", sum);
+
+            //先由程序随机生成一个1-100的整数，让用户猜。
+            //程序告诉用户猜大了还是猜小了，让用户再猜.....直到猜中为止。
+
+            Random r = new Random();
+            int a = r.Next(1, 101);
+            for (; ; )
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num == a)
+                {
+                    Console.WriteLine("猜中了！");
+                    break;
+                }
+                else if (num > a)
+                {
+                    Console.WriteLine("太大");
+                }
+                else
+                {
+                    Console.WriteLine("太小");
+                }
+            }
+
+            //打印菱形。
+            int line = int.Parse(Console.ReadLine());
+            int line_top = line / 2;
+            int line_bottom = line - line_top;
+
+            for (int i = 0; i < line_top; i++)
+            {
+                for (int j = 0; j < line + i; j++)
+                {
+                    if (i + j > line_top - 2 && j - i < line_top)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < line_bottom + 1; i++)
+            {
+                for (int j = 0; j < line_bottom * 3; j++)
+                {
+                    if (j - i >= 1 && i + j < line_bottom + 4)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Day5/BreakContinue/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                //Console.WriteLine(i);
+                if (i % 10 + i / 10 % 10 == 15)
+                {
+                    break;
+                }
+            }
+
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 10 + i / 10 % 10 == 15)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
