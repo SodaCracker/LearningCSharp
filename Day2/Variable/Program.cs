ed31d756e7fd6b25f8f98a0cad66d00b27774077
Wei Yue <genius0610@gmail.com>
2018-11-21 11:22:02 +0800

    Add files via upload

---
+++ Day2/Variable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentAge;
+            int totalCatCount;
+            float imageHeight;
+            int expectedFramesIn1Sec;
+        }
+    }
+}
